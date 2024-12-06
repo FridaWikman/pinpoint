d819e89768baf8718d2b428b629ea1de4751d1b5
FridaWikman <frida.wikman@hotmail.com>
2024-12-06 12:13:34 +0100

    Modified BDD-test.

--- frontend/cypress/e2e/addNote.feature
+++ frontend/cypress/e2e/addNote.feature
@@ -2,13 +2,7 @@ Feature: Add note
 
 There should be a button for 'add note'. When I click the button a modal should open. A form should exist in the modal with an input-field for title, a textfield for notes, a dropdown-meny for categories and an input-field for author name. There should also be a button for send form. I should be possible to type values in all fields and send the values on click at send-button. I will then check if my list of notes has increased ny one.
 
-Scenario: Open modal
+Scenario: Open modal, fill in form and add new note
 Given I am on the homepage and sees the button for add note
-When I click on the button
-Then The modal should be visible
-
-
-Scenario: Fill in form and add new note
-Given I see the modal and the form
-When I put values in all fields in the form. I click on send-button
+When I click on the button. I put values in all fields in the form. I click on send-button
 Then The modal should close and the list of notes should have increased by one
--- frontend/cypress/e2e/addNote.ts
+++ frontend/cypress/e2e/addNote.ts
@@ -13,42 +13,34 @@ Before(() => {
 
 Given('I am on the homepage and sees the button for add note', () => {
   cy.get('[data-cy=show-add-note-modal-button').should('be.visible')
-  cy.get('[data-cy=notes-list] .notes-list-item').then((items) => {
+  cy.get('[data-cy=notes-list-item]').then((items) => {
     initialLengthOfNotes = items.length
   })
 })
 
-Given('I see the modal and the form', () => {
-  cy.get('[data-cy=add-note-modal]').should('be-visable')
-  cy.get('data-cy=add-note-form').should('be.visible')
-})
-
-When('I click on the button', () => {
-  cy.get('[data-cy=show-add-note-modal-button]').click()
-})
-
-When('I put values in all fields in the form. I click on send-button', () => {
-  cy.get('[data-cy=add-note-title]').type('Till Kalle')
-  cy.get('[data-cy=add-note-note]').type(
-    'Glöm inte att vi ska hem till dina föräldrar i eftermiddag. Du behöver köpa en present.'
-  )
-  cy.get('[data-cy=add-note-categories-dropdown]').click()
-  cy.get('[data-cy=add-note-categories-dropdown] .dropdown-menu')
-    .contains('Personligt')
-    .click()
-  cy.get('[data-cy=add-note-author]').type('Anna')
-  cy.get('[data-cy=add-note-send-button]').click()
-})
-
-Then('The modal should be visible', () => {
-  cy.get('[data-cy=add-note-modal]').should('be.visible')
-})
+When(
+  'I click on the button. I put values in all fields in the form. I click on send-button',
+  () => {
+    cy.get('[data-cy=show-add-note-modal-button]').click()
+    cy.get('[data-cy=add-note-modal]').should('be.visible')
+    cy.get('[data-cy=add-note-title]').type('Till Kalle')
+    cy.get('[data-cy=add-note-note]').type(
+      'Glöm inte att vi ska hem till dina föräldrar i eftermiddag. Du behöver köpa en present.'
+    )
+    cy.get('[data-cy=add-note-categories-dropdown]').click()
+    cy.get('[data-cy=add-note-categories-dropdown] .dropdown-menu')
+      .contains('Personligt')
+      .click()
+    cy.get('[data-cy=add-note-author]').type('Anna')
+    cy.get('[data-cy=add-note-send-button]').click()
+  }
+)
 
 Then(
   'The modal should close and the list of notes should have increased by one',
   () => {
     cy.get('[data-cy=add-note-modal]').should('not.exist')
-    cy.get('[data-cy=notes-list] .notes-list-item').should(
+    cy.get('[data-cy=notes-list-item]').should(
       'have.length',
       initialLengthOfNotes + 1
     )
